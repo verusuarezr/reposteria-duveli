766dd26c44bd92788a1eaef5489cad69ae468806
veriuska <veriuskasuarez@gmail.com>
2022-08-24 18:01:11 -0300

    implementacion de git

---
+++ css/js/app.js
@@ -0,0 +1,11 @@
+/* const menu = document.querySelector(".menu");
+const openMenuMtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
+
+function toggleMenu( ){
+    menu.classList.toggle("menu_opened");
+
+}
+
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu); */
